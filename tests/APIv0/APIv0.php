2e851c529f15a5e80b5ae5b3bab00c4ab1e1b064
initvector <ryan.p@vanillaforums.com>
2017-04-27 17:58:31 -0400

    no config

--- tests/APIv0/APIv0.php
+++ tests/APIv0/APIv0.php
@@ -179,6 +179,9 @@ class APIv0 extends HttpClient {
 
         // Touch the config file because hhvm runs as root and we don't want the config file to have those permissions.
         $configPath = $this->getConfigPath();
+        if (file_exists($configPath)) {
+            die("Config should not exist: {$configPath}");
+        }
         touch($configPath);
         chmod($configPath, 0777);
         $apiKey = sha1(openssl_random_pseudo_bytes(16));
