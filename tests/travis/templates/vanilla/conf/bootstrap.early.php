6d6962f6d75c6effa42345a83ffb03f193080e8f
DaazKu <daazku@gmail.com>
2017-04-28 15:38:00 -0400

    More info but less greedy

--- tests/travis/templates/vanilla/conf/bootstrap.early.php
+++ tests/travis/templates/vanilla/conf/bootstrap.early.php
@@ -1,3 +1,7 @@
 <?php
-saveToConfig("SAVECONFIG_DEBUG_GRR", $_SERVER);
+saveToConfig("SAVECONFIG_DEBUG_GRR", [
+    'REQUEST_TIME' => $_SERVER['REQUEST_TIME'],
+    'REQUEST_URI' => $_SERVER['REQUEST_URI'],
+    'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'],
+]);
 // Common die
